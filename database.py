0fda14287c79c036d3efadea11e0ea4e88ff03e4
KbICb0 <108388709+KbICb0@users.noreply.github.com>
2026-01-13 16:44:56 +0300

    add sqlalchemy

--- database.py
+++ database.py
@@ -1,49 +1,31 @@
-import sqlite3
-# Устанавливаем соединение с базой данных
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
-DB_NAME = "support_tickets_sqlite.db"
+class Base(DeclarativeBase):
+    pass
 
-def get_connection():
-    connection = sqlite3.connect(DB_NAME)
-    cursor = connection.cursor()
-    return connection, cursor
-
-
-# Создаем таблицу Users
-def create_table_users():
-    conn, cursor = get_connection()
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Users (
-    id INTEGER PRIMARY KEY,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL,
-    role TEXT NOT NULL,
-    email TEXT NOT NULL
+class User(Base):
+    __tablename__ = "user_account"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+    addresses: Mapped[List["Address"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
     )
-    """)
-    conn.commit()
-    conn.close()
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
 
-#Вставка данных
-def create_user(id, username, password, role, email):
-    conn, cursor = get_connection()
-    cursor.execute("INSERT INTO Users VALUES (?, ?, ?, ?, ?)", (id, username, password, role, email))
-    conn.commit()
-    conn.close()
-
-#Запрос данных
-def get_users():
-    conn, cursor = get_connection()
-    cursor.execute("""
-    SELECT * FROM Users
-    """)
-    result = cursor.fetchall()
-    print(result)
-    conn.commit()
-    conn.close()
-    
-
-
-if __name__ == "__main__":
-    create_user(1, 'admin', 'admin', 'admin', 'admin@example.com')
-    get_users()
+class Address(Base):
+    __tablename__ = "address"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped["User"] = relationship(back_populates="addresses")
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
